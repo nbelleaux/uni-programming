7cf67164e73f0bf7cd9396bbb683ebfb4c8ffea3
nbelleaux <natrylanceward@protonmail.com>
2025-10-14 12:34:18 +0100

    Week 1 lab complete

---
+++ week1_lab.py
@@ -0,0 +1,13 @@
+# Storing strings
+student_name = "Alex"
+
+# Storing an integer
+student_age = 20
+
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+# Performing arithmetic operations
+next_year_age = student_age + 1
+print("Age next year:", next_year_age)
