05a84ad540a16b6ed7a5988205600dc2750ee3a1
nbelleaux <natrylanceward@protonmail.com>
2025-10-21 14:32:54 +0100

    Part 2

---
+++ week3_lab2.py
@@ -0,0 +1,14 @@
+for row in range(5):
+    for col in range(5):
+        print("*", end=" ")
+
+    print("")
+
+
+
+rows2 = 5
+for i in range(rows2 + 1):
+    for j in range(i):
+        print("*", end=" ")
+
+    print("")
